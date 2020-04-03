f6f6a47f4df3b79889a0860b58fa98238264069d
Jacob Steinhardt <jsteinhardt@berkeley.edu>
2020-04-02 21:18:25 -0700

    add script to pull gobal time series

---
+++ pull_global.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
+mv time_series_covid19_deaths_global.csv data/
